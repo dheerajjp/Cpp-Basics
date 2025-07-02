bc7222f4a5356c3cff2c14e31331691ef8e9990a
dheerajjp <150685227+dheerajjp@users.noreply.github.com>
2025-07-02 15:40:14 +0530

    Create q1.cpp

---
+++ 2 July/q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int day, bill;
+    cin >> day >> bill;
+
+    int lastDigitOfDay = day % 10;
+
+    if (bill % day == 0 || bill % 10 == lastDigitOfDay)
+        cout << "yes";
+    else
+        cout << "no";
+
+    return 0;
+}
