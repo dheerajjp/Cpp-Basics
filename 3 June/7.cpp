cda67c0073e1e6f0822f72606b5820e25b7f04ac
dheerajjp <150685227+dheerajjp@users.noreply.github.com>
2025-07-03 19:41:21 +0530

    Create 7.cpp

---
+++ 3 June/7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printpattern7(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0; j<2*i+1; j++){
+            cout<<"*";
+        }//space
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int a;
+    cin>>a;
+    printpattern7(a);
+}
